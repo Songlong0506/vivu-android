46a6d59e79d5d484c1948b72efcf51b96df750da
Songlong0506 <34188445+Songlong0506@users.noreply.github.com>
2025-09-14 15:25:52 +0700

    feat: add multilingual support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'screens/map_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const TopRatedPlacesApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('vi'), Locale('ja')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const TopRatedPlacesApp(),
+    ),
+  );
 }
 
 class TopRatedPlacesApp extends StatelessWidget {
@@ -14,10 +23,13 @@ class TopRatedPlacesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Top Rated Places',
+      title: 'app_title'.tr(),
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
       home: const MapScreen(),
       debugShowCheckedModeBanner: false,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
+      locale: context.locale,
     );
   }
 }
